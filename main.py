8ca35c5bc54f09aefe29b2f97cba7fdd61eb0e8e
EnnaLea <soleariaeve@gmail.com>
2024-12-02 10:19:12 +0100

    implemented sqlite database

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import selectorlib
 from datetime import datetime
+import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect("data_temperature.db")
 
 now = datetime.now()
 
@@ -19,10 +22,12 @@ def extract(source):
     return value
 
 def store(extracted):
-    date = now.strftime("%d-%m-%Y %H:%M:%S")
-    with open("data.txt", "a") as file:
-        line = f"{date},{extracted}\n"
-        return file.write(line)
+    date = now.strftime("%d-%m-%Y %H:%M:%S").strip()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperatures VALUES(?, ?)", (date, extracted))
+    connection.commit()
+
+
 
 
 
